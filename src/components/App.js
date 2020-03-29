4e57841950359a0ee475d43f35c3a1da1828a2b3
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-29 12:34:32 -0400

    First commit

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Container from "./Container"
+import Svg from "./Svg"
+
+function App() {
+  return (
+    <Container>
+      <Svg width={800} height={800}>
+        {/* Insert components here */}
+      </Svg>
+    </Container>
+  );
+}
+
+export default App;
