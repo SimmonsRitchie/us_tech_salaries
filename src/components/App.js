790cb511a67ce03864830754e16cf99fdde0d34c
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-29 19:52:20 -0400

    Wire up choropleth map

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,68 @@
-import React, {Component} from 'react';
-import * as d3 from 'd3'
-import _ from 'lodash'
-import Preloader from "./Preloader"
-import { loadAllData} from "../DataHandling"
+import React, { Component } from "react";
+import * as d3 from "d3";
+import _ from "lodash";
+import Preloader from "./Preloader";
+import { loadAllData } from "../DataHandling";
+import CountyMap from "./CountyMap";
 
 class App extends Component {
   state = {
     techSalaries: [],
     medianIncomes: [],
     countyNames: []
-  }
+  };
 
   componentDidMount() {
-    loadAllData( data => this.setState(data))
+    loadAllData(data => this.setState(data));
+  }
+
+  countyValue(county, techSalariesMap) {
+    const medianHousehold = this.state.medianIncomes[county.id],
+      salaries = techSalariesMap[county.name];
+
+    if (!medianHousehold || !salaries) {
+      return null;
+    }
+
+    const median = d3.median(salaries, d => d.base_salary);
+
+    return {
+      countyID: county.id,
+      value: median - medianHousehold.medianIncome
+    };
   }
 
   render() {
-    const { techSalaries} = this.state
+    const { techSalaries, countyNames, usTopoJson, USstateNames } = this.state;
     if (techSalaries.length < 1) {
-      return <Preloader />
+      return <Preloader />;
     }
+
+    const filteredSalaries = techSalaries,
+      filteredSalariesMap = _.groupBy(filteredSalaries, "countyID"),
+      countyValues = countyNames
+        .map(county => this.countyValue(county, filteredSalariesMap))
+        .filter(d => !_.isNull(d));
+
+    let zoom = null;
+
     return (
       <div className="App container">
-      <h1>Loaded {techSalaries.length} salaries</h1>
+        <svg width="1100" height="500">
+          <CountyMap
+            usTopoJson={usTopoJson}
+            USstateNames={USstateNames}
+            values={countyValues}
+            x={0}
+            y={0}
+            width={500}
+            height={500}
+            zoom={zoom}
+          />
+        </svg>
       </div>
     );
-
   }
-
 }
 
 export default App;
